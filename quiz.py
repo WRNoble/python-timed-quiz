0ae3de6f9378fd4153a6efdaf63a1b7fc90ea430
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-02 17:03:36 -0400

    establishing question logic

--- quiz.py
+++ quiz.py
@@ -8,6 +8,12 @@ class Quiz:
     answer = []
 
     def __init__(self):
+        question_types = (Add, Multiply)
+        for _ in range (10):
+            num1 = random.randint(1, 10)
+            num2 = random.randint(1, 10)
+            question = question_types
+            self.questions.append()
 
 
     def take_quiz(self):
@@ -16,6 +22,12 @@ class Quiz:
     def ask(self, question):
 
     def total_corret(self):
+        total = 0
+        for answer in self.answers:
+            if answer[0]:
+                total += 1
+            return total
 
     def summary(self):
         print("You got {} out of {} correct!".format(self.total_correct(), len(self.questions)))
+        print("it took you {} seconds total".format((self.end_time-self.start_time).seconds))
