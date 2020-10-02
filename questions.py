c7432d5f39e638a96f4475abf1107045a6b51ca5
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-02 16:44:03 -0400

    added multiply class

--- questions.py
+++ questions.py
@@ -2,7 +2,12 @@ class Question:
     answer = None
     text = None
 
-class Add:
+class Add(Question):
     def __init__(self, num1, num2):
         self.text = '{} + {}'.format(num1, num2)
         self.answer = num1 + num2
+
+class Multiply(Question):
+    def __init__(self, num1, num2):
+        self.text = '{} + {}'.format(num1, num2)
+        self.answer = num1 * num2
