34d149387f091568c0ee25d2f1ec36d08cdb38b3
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-02 16:39:53 -0400

    second class added

--- questions.py
+++ questions.py
@@ -1,3 +1,8 @@
 class Question:
     answer = None
     text = None
+
+class Add:
+    def __init__(self, num1, num2):
+        self.text = '{} + {}'.format(num1, num2)
+        self.answer = num1 + num2
