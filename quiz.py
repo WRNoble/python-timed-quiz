6a1196c5b93c5fb68f641111268f9a5b0107d68a
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-02 16:50:39 -0400

    filled out quiz class a little more

--- quiz.py
+++ quiz.py
@@ -5,3 +5,14 @@ from questions import Add, Multiply
 
 class Quiz:
     questions = []
+    answer = []
+
+    def __init__(self):
+
+
+    def take_quiz(self):
+
+    
+    def ask(self, question):
+
+    def summary(self):
